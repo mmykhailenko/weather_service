682342257fdffe223ad3f1103bb94943ad60a4e5
Maksim Mikhailenko <mmykhailenko@lohika.com>
2019-03-21 21:13:20 +0200

    Added weather_get and weather_set views

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Weather(models.Model):
+    city_name = models.TextField(max_length=100)
+    cord_lon = models.TextField()
+    cord_lat = models.TextField()
+    weather_main = models.TextField()
+    weather_description = models.TextField()
+    temp = models.TextField()
+    date_time = models.TextField()
--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from weather.serializer import UserSerializer, GroupSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+import requests
+
+from weather.models import Weather
+from weather.serializer import UserSerializer, GroupSerializer, WeatherSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -17,3 +23,37 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+
+@api_view(['GET'])
+def weather_save(request, city):
+    API_KEY = "6034d87efaa342b60bd74f470f24eb86"
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city, API_KEY)
+
+    if request.method == 'GET':
+        resp = requests.get(url).json()
+
+        res = {
+            "city_name": resp.get("name"),
+            "cord_lon": resp["coord"]["lon"],
+            "cord_lat": resp["coord"]["lat"],
+            "weather_main": resp["weather"][0]["main"],
+            "weather_description": resp["weather"][0]["main"],
+            "date_time": resp.get("dt"),
+            "temp": resp["main"]["temp"]
+        }
+
+        serializer = WeatherSerializer(data=res)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def weather_get(request):
+    if request.method == 'GET':
+        weather = Weather.objects.all()
+        serializer = WeatherSerializer(weather, many=True)
+        return Response(serializer.data)
